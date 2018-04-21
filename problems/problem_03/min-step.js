72ffd93e800dafc2fe14df0974e48857f936f4bc
Perajit Pasupat <perajit.p@gmail.com>
2018-04-21 23:32:31 +0700

    Solve problem_03

--- problems/problem_03/min-step.js
+++ problems/problem_03/min-step.js
@@ -1,5 +1,28 @@
+/**
+  Assume f(n) = the function we want
+
+  if n is even: f(n) = f(n / 2) + 1
+  if n is odd: f(n) = maxOf{
+    f(n - 1) + 1 -> for decreasing,
+    f((n + 1) / 2) + 1 + 1 -> for increasing
+  }
+*/
 const minStep = input => {
-  // put your code here !!
-  return 0;
+  let result;
+
+  if (input <= 1) {
+    result = 0;
+  }
+  else if (!(input % 2)) {
+    result = minStep(input / 2) + 1;
+  }
+  else {
+    let downVal = minStep(input - 1) + 1;
+    let upVal = minStep((input + 1) / 2) + 2;
+    result = Math.min(downVal, upVal);
+  }
+
+  return result;
 };
+
 module.exports = { minStep };
